27e0b7f337446f8f10a502971e9a3d5e182024c5
kaedenakai <kaedenakai1115@gmail.com>
2024-12-16 20:11:08 +0900

    UPDATE 2.0

--- js/companies.js
+++ js/companies.js
@@ -1,37 +1,39 @@
 const companies = [
     {
         id: 1,
-        name: "株式会社〇〇",
-        description: "森林保全活動の取り組み",
+        title: "株式会社〇〇",
+        description: "海洋環境保全プロジェクトA",
         image: "img/kigyou.png",
-        activity: "地域の森林整備による炭素吸収量の増加",
+        activity: "海洋環境保全プロジェクトA",
         date: "2024年3月15日",
         location: "岐阜県飛騨市",
-        details: "株式会社〇〇は、岐阜県飛騨市で持続可能な森林経営を推進しています。地元の林業者と協力し、年間1万tのCO2削減を目指します。",
+        amount: "10t-CO2",
+        details: "自社製造のXXによる炭素排出削減活動",
         link: "https://example.com"
     },
     {
         id: 2,
-        name: "△△株式会社",
-        description: "森林管理プロジェクト",
+        title: "△△株式会社",
+        description: "水質管理プロジェクト",
         image: "img/kigyou.png",
-        activity: "地域の森林整備による炭素吸収量の増加",
+        activity: "水質管理プロジェクト",
         date: "2024年3月15日",
         location: "岐阜県飛騨市",
-        details: "株式会社〇〇は、岐阜県飛騨市で持続可能な森林経営を推進しています。地元の林業者と協力し、年間1万tのCO2削減を目指します。",
+        amount: "0.8t-CO2",
+        details: "自社製造のXXによる炭素排出削減活動",
         link: "https://example.com"
     },
         {
-        id: 2,
-        name: "△△株式会社",
-        description: "森林管理プロジェクト",
+        id: 3,
+        title: "△△株式会社",
+        description: "ガンガゼウニ捕獲プロジェクト",
         image: "img/kigyou.png",
-        activity: "地域の森林整備による炭素吸収量の増加",
-        date: "2024年3月15日",
-        location: "岐阜県飛騨市",
-        details: "株式会社〇〇は、岐阜県飛騨市で持続可能な森林経営を推進しています。地元の林業者と協力し、年間1万tのCO2削減を目指します。",
+        activity: "ガンガゼウニ捕獲プロジェクト",
+        date: "2024年6月25日",
+        location: "沖縄県那覇市",
+        amount: "13t-CO2",
+        details: "海洋環境保護プロジェクトへの参画",
         link: "https://example.com"
     }
-
     // 新しい企業活動はここに追加
 ];
