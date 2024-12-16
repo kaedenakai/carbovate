12e33b14f1e9cbb1c15850f6824a718df029b8b1
kaedenakai <kaedenakai1115@gmail.com>
2024-12-16 10:42:02 +0900

    UPDATE 1.0

---
+++ js/companies.js
@@ -0,0 +1,37 @@
+const companies = [
+    {
+        id: 1,
+        name: "株式会社〇〇",
+        description: "森林保全活動の取り組み",
+        image: "img/kigyou.png",
+        activity: "地域の森林整備による炭素吸収量の増加",
+        date: "2024年3月15日",
+        location: "岐阜県飛騨市",
+        details: "株式会社〇〇は、岐阜県飛騨市で持続可能な森林経営を推進しています。地元の林業者と協力し、年間1万tのCO2削減を目指します。",
+        link: "https://example.com"
+    },
+    {
+        id: 2,
+        name: "△△株式会社",
+        description: "森林管理プロジェクト",
+        image: "img/kigyou.png",
+        activity: "地域の森林整備による炭素吸収量の増加",
+        date: "2024年3月15日",
+        location: "岐阜県飛騨市",
+        details: "株式会社〇〇は、岐阜県飛騨市で持続可能な森林経営を推進しています。地元の林業者と協力し、年間1万tのCO2削減を目指します。",
+        link: "https://example.com"
+    },
+        {
+        id: 2,
+        name: "△△株式会社",
+        description: "森林管理プロジェクト",
+        image: "img/kigyou.png",
+        activity: "地域の森林整備による炭素吸収量の増加",
+        date: "2024年3月15日",
+        location: "岐阜県飛騨市",
+        details: "株式会社〇〇は、岐阜県飛騨市で持続可能な森林経営を推進しています。地元の林業者と協力し、年間1万tのCO2削減を目指します。",
+        link: "https://example.com"
+    }
+
+    // 新しい企業活動はここに追加
+];
